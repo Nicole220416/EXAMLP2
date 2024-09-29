914063f3273f6cd5ec232a4d324bab12a1d497a1
Nicole220416 <nicolesollangelgalindodelacruz@gmail.com>
2024-09-29 11:29:09 -0500

    Initial commit

---
+++ src/main/java/com/example/subject_management/service/SubjectService.java
@@ -0,0 +1,34 @@
+package com.example.subject_management.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.subject_management.model.Subject;
+import com.example.subject_management.repository.SubjectRepository;
+import java.util.List;
+
+
+@Service
+public class SubjectService {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    public List<Subject> findAllSubjects() {
+        return subjectRepository.findAll();
+    }
+
+    public Optional<Subject> findSubjectById(int id) {
+        return subjectRepository.findById(id);
+    }
+
+    public Subject saveSubject(Subject subject) {
+        return subjectRepository.save(subject);
+    }
+
+    public void deleteSubject(int id) {
+        subjectRepository.deleteById(id);
+    }
+}
